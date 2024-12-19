95021c880b68fc08ee7d7dae914b882ff61e082d
DamCo <73948784+Damdam86@users.noreply.github.com>
2024-12-19 15:49:29 +0100

    Modification mots clés

--- search_movies.py
+++ search_movies.py
@@ -121,6 +121,7 @@ max_vote_average = dtt.get_max_vote_average(movies_list)
 col1, col2, col3 = st.columns([4,1,4])
 
 with col1: 
+    st.dataframe(data=movies_list)
     selected_genre = st.multiselect("Filtrez par genre :", genres)
     selected_year = st.slider("Sélectionner une plage d'années", most_older_year, most_recent_year, (most_older_year, most_recent_year))
 
