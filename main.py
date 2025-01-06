f83447bc4eb42cf436f05576f42aaf0f9a51618e
DamCo <73948784+Damdam86@users.noreply.github.com>
2025-01-06 15:23:33 +0100

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ pages = [
     st.Page("project.py", title="Le projet", icon="🔥"),
     st.Page("team.py", title="L'équipe", icon="🤹‍♀️"),
     st.Page("analyse.py", title="Analyse", icon="📊"),
-    st.Page("intro.py", title="intro", icon="🎭"),
+    st.Page("intro.py", title="Intro", icon="🎭"),
     st.Page("reco.py", title="Recommandation", icon="🏠"),
     st.Page("search_movies.py", title="Rechercher", icon="🎬"),
     st.Page("movie.py", title="Les films", icon="🎬"),
